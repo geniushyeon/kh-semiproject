6cdc5459f679f677bc92100919d8cc07e5bba00f
yoon <jmjm5412@gmail.com>
2021-01-03 16:49:13 +0900

    JYJ - servlet add

---
+++ backend/Coffeesazo/src/com/coffeesazo/notice/controller/NoticeServlet.java
@@ -0,0 +1,56 @@
+package com.coffeesazo.notice.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.coffeesazo.notice.model.service.NoticeService;
+import com.coffeesazo.notice.model.vo.NoticeVo;
+
+/**
+ * Servlet implementation class NoticeServlet
+ */
+@WebServlet("/notice")
+public class NoticeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public NoticeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		
+		HttpSession session = request.getSession(); //나 세션을 가지고올래
+		String memberid = ((String)session.getAttribute("id"));
+		
+		ArrayList<NoticeVo> noticeList = new NoticeService().selectNoticeList(memberid);
+	
+		if(!noticeList.isEmpty()) {
+			request.setAttribute("noticeList", noticeList);
+			RequestDispatcher view = request.getRequestDispatcher("view/notice/notice.jsp");
+			view.forward(request, response);
+			System.out.println(noticeList);
+		} else {
+			response.sendRedirect("");
+		}
+		
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
