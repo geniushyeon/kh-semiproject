e1ae80a4aea3976cdc10a76d88aee715dbbda2f3
St4rFi5h <jeremy96kr@naver.com>
2021-01-05 14:31:24 +0900

    PJW SERVELT doing

--- backend/Coffeesazo/src/mypage/controller/OrderListSelectServlet.java
+++ backend/Coffeesazo/src/mypage/controller/OrderListSelectServlet.java
@@ -1,11 +1,17 @@
 package mypage.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import mypage.model.service.OrderService;
+import mypage.model.vo.OrderViewList;
 
 /**
  * Servlet implementation class OrderListSelectServlet
@@ -27,7 +33,11 @@ public class OrderListSelectServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		HttpSession session = request.getSession(); 
+		String memberid = ((String)session.getAttribute("id"));
+		ArrayList<OrderViewList> odlist = new OrderService().SelectOrderList(memberid);
 	}
 
 	/**
